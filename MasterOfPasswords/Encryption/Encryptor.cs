4bce23242da6befee138f5eccdaa2b9dbd7f16bd
Fimeru <fimerur@gmail.com>
2024-11-08 12:25:55 +0300

    test structure

--- MasterOfPasswords/Encryption/Encryptor.cs
+++ MasterOfPasswords/Encryption/Encryptor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -5,16 +7,30 @@ namespace MasterOfPasswords.Encryption;
 
 public class Encryptor : IEncryptor
 {
+    private IEncryptor _encryptorImplementation;
+
     public string Encrypt(string plainText, string key)
     {
-        using (RijndaelManaged rijndael = new RijndaelManaged())
+        using (var rijndael = new RijndaelManaged())
         {
-            rijndael.Key = Encoding.UTF8.GetBytes(key.PadRight(32));
-            rijndael.IV = new byte[16];
+            rijndael.GenerateIV();
+                
+            byte[] salt = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+
+            rijndael.Key = GenerateKey(key, salt);
+            rijndael.Mode = CipherMode.CBC;
+            rijndael.Padding = PaddingMode.PKCS7;
 
             using (var encryptor = rijndael.CreateEncryptor(rijndael.Key, rijndael.IV))
             using (var ms = new MemoryStream())
             {
+                ms.Write(salt, 0, salt.Length);
+                ms.Write(rijndael.IV, 0, rijndael.IV.Length);
+
                 using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 using (var sw = new StreamWriter(cs))
                 {
@@ -28,18 +44,36 @@ public class Encryptor : IEncryptor
 
     public string Decrypt(string encryptedText, string key)
     {
-        using (RijndaelManaged rijndael = new RijndaelManaged())
+        byte[] fullCipher = Convert.FromBase64String(encryptedText);
+
+        using (var rijndael = new RijndaelManaged())
         {
-            rijndael.Key = Encoding.UTF8.GetBytes(key.PadRight(32));
-            rijndael.IV = new byte[16];
+            byte[] salt = new byte[16];
+            byte[] iv = new byte[rijndael.BlockSize / 8];
+            
+            Array.Copy(fullCipher, 0, salt, 0, salt.Length);
+            Array.Copy(fullCipher, salt.Length, iv, 0, iv.Length);
+
+            rijndael.Key = GenerateKey(key, salt);
+            rijndael.IV = iv;
+            rijndael.Mode = CipherMode.CBC;
+            rijndael.Padding = PaddingMode.PKCS7;
 
             using (var decryptor = rijndael.CreateDecryptor(rijndael.Key, rijndael.IV))
-            using (var ms = new MemoryStream(Convert.FromBase64String(encryptedText)))
+            using (var ms = new MemoryStream(fullCipher, salt.Length + iv.Length, fullCipher.Length - salt.Length - iv.Length))
             using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
             using (var sr = new StreamReader(cs))
             {
                 return sr.ReadToEnd();
             }
+        }
+    }
+
+    private byte[] GenerateKey(string key, byte[] salt)
+    {
+        using (var deriveBytes = new Rfc2898DeriveBytes(key, salt, 10000))
+        {
+            return deriveBytes.GetBytes(16);
         }
     }
 }
